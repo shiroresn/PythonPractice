9858d92a4bbaea44386d8a15024ff1f3a328fcda
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-11-16 18:11:15 +0530

    Create PracticeList.py

---
+++ PracticeList.py
@@ -0,0 +1,5 @@
+list = ["Shubham","Kalpesh",77.65,98.20,22,21]
+
+print list[0]+" scored "+str(list[2])+"% and his age is "+str(list[4])+" years."
+
+print list[1]+" scored "+str(list[3])+"% and his age is "+str(list[5])+" years."
