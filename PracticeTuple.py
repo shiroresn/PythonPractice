b55a29ee9b47454d4320be1c026388287cff5b21
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-11-16 18:15:44 +0530

    Create PracticeTuple.py

---
+++ PracticeTuple.py
@@ -0,0 +1,5 @@
+tuple = ("Shubham","Kalpesh",77.65,98.20,22,21)
+
+print tuple[0]+" scored "+str(tuple[2])+"% and his age is "+str(tuple[4])+" years."
+
+print tuple[1]+" scored "+str(tuple[3])+"% and his age is "+str(tuple[5])+" years."
