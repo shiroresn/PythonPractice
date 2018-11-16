d54d46a8f20ffd82260c27dd832a29c27884a5de
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-11-16 18:03:37 +0530

    Create FirstProgram.py

---
+++ FirstProgram.py
@@ -0,0 +1,7 @@
+fname = "Shubham"
+lname = "Shirore"
+mobile = "8087163406"
+age = 22
+marks = 98.23
+
+print fname,lname+" has "+str(marks)+"% Marks. His mobile number is "+mobile+" and age is "+str(age)
