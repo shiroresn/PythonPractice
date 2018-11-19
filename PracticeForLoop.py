cee1e01d37b76c00ba7599a5c8f20e1c5709949b
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-11-19 14:19:32 +0530

    Create PracticeForLoop.py

---
+++ PracticeForLoop.py
@@ -0,0 +1,11 @@
+#Example 1
+count = 0
+for letter in "Python" :
+    print "Current letter : ",count,letter
+    count=count+1
+    
+#Example 2
+cities = ['Mumbai','Pune','Nashik','Nagpur']
+for city in cities:
+    print "City Name :",city
+
