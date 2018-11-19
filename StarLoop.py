1e35f293d5ed1fb86f623ab63a66f979e40e8b7c
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-11-19 14:51:54 +0530

    Update StarLoop.py

--- StarLoop.py
+++ StarLoop.py
@@ -7,10 +7,10 @@ count = 5
 xx = 1
 for j in range(0,5):
     for i in range(0,count):
-        print " ",
+        print "",
     for k in range(0,xx):
-        print "* ",
-        xx=xx+1
+        print "*",
         
+    xx=xx+1   
     print ""
     count = count-1
